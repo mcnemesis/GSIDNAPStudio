52764f5e9c45c6f800077882d0e6fdeea6384a6d
Nemesis Fixx <joewillrich@gmail.com>
2016-10-04 12:17:05 +0000

    refactoring and styling

--- dist/formbuilder.js
+++ dist/formbuilder.js
@@ -642,7 +642,7 @@
         IMPORT_PERSONA_FILE: 'Import from FILE',
         DOWNLOAD_PERSONA: 'Download The Persona',
         SET_THEATRE_URI: 'Theatre Address',
-        SET_TRANSPORT_MODE: 'How the Historion will submit acts',
+        SET_TRANSPORT_MODE: 'How the Histrion will submit acts',
         EMPTY_FIELDS_MESSAGE: 'The app is currently empty! Start designing, by dragging and dropping fields onto the app canvas below...'
       }
     };
